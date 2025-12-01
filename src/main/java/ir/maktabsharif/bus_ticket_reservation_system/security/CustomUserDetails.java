f4abd6b035ad19b6a688eb1d61f1a5735b832f3a
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-01 13:40:29 +0330

    feat:create CustomUserDetails to wrap user entity object

---
+++ src/main/java/ir/maktabsharif/bus_ticket_reservation_system/security/CustomUserDetails.java
@@ -0,0 +1,36 @@
+package ir.maktabsharif.bus_ticket_reservation_system.security;
+
+import ir.maktabsharif.bus_ticket_reservation_system.model.User;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class CustomUserDetails implements UserDetails {
+
+    private final User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(()->"Role_"+user.getRole().name());
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+
+}
