bd35a87afabcf93e18d1e6669bdfe2b09ff82f8b
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-01 14:03:42 +0330

    fix:change the return type of findByUsername method in UserRepository

--- src/main/java/ir/maktabsharif/bus_ticket_reservation_system/repository/UserRepository.java
+++ src/main/java/ir/maktabsharif/bus_ticket_reservation_system/repository/UserRepository.java
@@ -4,7 +4,8 @@ import ir.maktabsharif.bus_ticket_reservation_system.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User,Long> {
-    List<User> findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
