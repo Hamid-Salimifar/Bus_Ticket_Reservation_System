b6a5bd950a908defa1e647897116a96e0e71d956
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-02 00:29:52 +0330

    feat: add register method in UserService

--- src/main/java/ir/maktabsharif/bus_ticket_reservation_system/service/UserService.java
+++ src/main/java/ir/maktabsharif/bus_ticket_reservation_system/service/UserService.java
@@ -3,4 +3,5 @@ package ir.maktabsharif.bus_ticket_reservation_system.service;
 import ir.maktabsharif.bus_ticket_reservation_system.model.User;
 
 public interface UserService extends BaseService<User> {
+    void register(User user);
 }
--- src/main/java/ir/maktabsharif/bus_ticket_reservation_system/service/impl/UserServiceImpl.java
+++ src/main/java/ir/maktabsharif/bus_ticket_reservation_system/service/impl/UserServiceImpl.java
@@ -1,15 +1,34 @@
 package ir.maktabsharif.bus_ticket_reservation_system.service.impl;
 
+import ir.maktabsharif.bus_ticket_reservation_system.model.Role;
 import ir.maktabsharif.bus_ticket_reservation_system.model.User;
 import ir.maktabsharif.bus_ticket_reservation_system.repository.UserRepository;
 import ir.maktabsharif.bus_ticket_reservation_system.service.UserService;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl extends BaseServiceImpl<User> implements UserService {
 
-    public UserServiceImpl(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         super(userRepository);
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+
+    }
+
+    @Override
+    public void register(User user) {
+        User finalUser = User.builder()
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(passwordEncoder.encode(user.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(finalUser);
     }
 }
