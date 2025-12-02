4be00c3faff850a87d46f72416567cde7ae6a702
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-02 17:05:37 +0330

    feat: add searchTrip and findByUsername methods in TripService and UserService interfaces

--- src/main/java/ir/maktabsharif/bus_ticket_reservation_system/service/UserService.java
+++ src/main/java/ir/maktabsharif/bus_ticket_reservation_system/service/UserService.java
@@ -2,6 +2,9 @@ package ir.maktabsharif.bus_ticket_reservation_system.service;
 
 import ir.maktabsharif.bus_ticket_reservation_system.model.User;
 
+import java.util.Optional;
+
 public interface UserService extends BaseService<User> {
     void register(User user);
+    Optional<User> findByUsername(String username);
 }
