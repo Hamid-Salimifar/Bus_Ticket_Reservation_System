aff07573e22aeb73c9532282a14975f29d8eca73
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-02 17:01:19 +0330

    feat: add a method to find a trip by route and date in TripRepository

--- src/main/java/ir/maktabsharif/bus_ticket_reservation_system/repository/TripRepository.java
+++ src/main/java/ir/maktabsharif/bus_ticket_reservation_system/repository/TripRepository.java
@@ -3,5 +3,11 @@ package ir.maktabsharif.bus_ticket_reservation_system.repository;
 import ir.maktabsharif.bus_ticket_reservation_system.model.Trip;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface TripRepository extends JpaRepository<Trip,Long> {
+
+    List<Trip> findByDepartureLocationAndDestinationLocationAndDepartureDateOrderByDepartureTimeDesc(String departureLocation, String destinationLocation, LocalDate departureDate);
+
 }
