307c89dd2297d69cf58957443814799688b22f5e
Hamid-Salimifar <hamidsalimifar618@gmail.com>
2025-12-02 17:00:33 +0330

    feat: add findByUser method in TicketRepository

--- src/main/java/ir/maktabsharif/bus_ticket_reservation_system/repository/TicketRepository.java
+++ src/main/java/ir/maktabsharif/bus_ticket_reservation_system/repository/TicketRepository.java
@@ -1,7 +1,11 @@
 package ir.maktabsharif.bus_ticket_reservation_system.repository;
 
 import ir.maktabsharif.bus_ticket_reservation_system.model.Ticket;
+import ir.maktabsharif.bus_ticket_reservation_system.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TicketRepository extends JpaRepository<Ticket,Long> {
+    List<Ticket> findByUser(User user);
 }
